4beb1ded3bda2bf1adc8efc609ae60e85c1426a3
FRY Developer <alexiusandromedavsgalaxia@gmail.com>
2026-09-13 23:14:19 +0200

    feat(framework): export ARR platform APIs

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 export { Lexer } from './lexer.js';
 export { Parser } from './parser.js';
 export { Runtime } from './runtime.js';
+export { ARRUI, ARRAudio, ARRGraphics, ARRInput, ARRStorage, ARRTime, createFramework } from './framework.js';
+export type { UINode, Vec2, Rect, ARRFramework } from './framework.js';
 export type * from './ast.js';
