f0548263db6b0fcee8543c60057972faeafae693
FRY Developer <alexiusandromedavsgalaxia@gmail.com>
2026-09-16 14:15:14 +0200

    feat(native): export freestanding compiler backend

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export { Lexer } from './lexer.js';
 export { Parser } from './parser.js';
 export { Runtime } from './runtime.js';
+export { compileX86Bios } from './native.js';
 export { ARRUI, ARRAudio, ARRGraphics, ARRInput, ARRStorage, ARRTime, createFramework } from './framework.js';
 export type { UINode, Vec2, Rect, ARRFramework } from './framework.js';
 export type * from './ast.js';
